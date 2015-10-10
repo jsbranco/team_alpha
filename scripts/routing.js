be412a638d383175a7b0b3feb048c4b606025745
Justin Ling <justin@appedu.co>
2015-10-10 22:53:25 +0800

    added auth

--- scripts/routing.js
+++ scripts/routing.js
@@ -2,17 +2,39 @@ Router.configure({
   layoutTemplate: "nav-main-layout"
 });
 
-Router.route('/', function () {
-  this.render('search');
+Router.plugin("auth", {
+  except: [
+    "login",
+    "register"
+  ]
 });
 
+Router.route('/');
+
 Router.route('/addLocation');
+
 Router.route("/home");
+
 Router.route("/landing");
+
 Router.route("/login", {
   layoutTemplate: "no-layout"
 });
+
+Router.route("/logout", {
+  name: "logout",
+  action: function() {
+    Meteor.logout(function() {
+      Router.go("/login");
+    });
+  },
+  onAfterAction: function() {
+    document.title = "Logout";
+  }
+});
+
 Router.route("/register", {
   layoutTemplate: "no-layout"
 });
+
 Router.route("/search");
