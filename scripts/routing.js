4d5d34278dd9cd868b8ade0e6c908eb3858be869
juniobranco <Br@anco04>
2015-10-11 10:48:58 +0800

    Adding Sanitizing to Registration

--- scripts/routing.js
+++ scripts/routing.js
@@ -3,71 +3,95 @@ Router.configure({
 });
 
 Router.plugin("auth", {
-  except: [
-    "login",
-    "register"
-  ]
+    except: [
+        "login",
+        "register",
+        "forgotPassword",
+        "resetPassword"
+    ]
 });
 
 Router.route('/', {
-  action: function () {
-    this.render('search');
-  },
-  onAfterAction: function() {
-    document.title = "Location Bookmark";
-  }
+    action: function () {
+        this.render('search');
+    },
+    onAfterAction: function () {
+        document.title = "Location Bookmark";
+    }
 });
 
 Router.route("/bookmarks", {
-  action: function() {
-    this.render("search");
-  },
-  data: function() {
-    return "bookmarks";
-  }
+    action: function () {
+        this.render("search");
+    },
+    data: function () {
+        return "bookmarks";
+    }
 });
 
 Router.route("/login", {
-  layoutTemplate: "no-layout",
-  onAfterAction: function() {
-    document.title = "Login";
-  }
+    layoutTemplate: "no-layout",
+    onAfterAction: function () {
+        document.title = "Login";
+    }
 });
 
+
 Router.route("/logout", {
-  action: function() {
-    Meteor.logout(function() {
-      Router.go("/login");
-    });
-  },
-  onAfterAction: function() {
-    document.title = "Logout";
-  }
+    action: function () {
+        Meteor.logout(function () {
+            Router.go("/login");
+        });
+    },
+    onAfterAction: function () {
+        document.title = "Logout";
+    }
 });
 
 Router.route("/register", {
-  layoutTemplate: "no-layout",
-  onAfterAction: function() {
-    document.title = "Register";
-  }
+    layoutTemplate: "no-layout",
+    onAfterAction: function () {
+        document.title = "Register";
+    }
+});
+Router.route("/resetPassword/:resetPasswordToken", {
+    layoutTemplate: "no-layout",
+    name: "resetPassword",
+    onBeforeAction: function () {
+        if (Meteor.userId()) {
+            this.redirect('/search')
+        } else {
+            this.next();
+        }
+        Accounts._resetPasswordToken = this.params.resetPasswordToken
+    },
+    onAfterAction: function () {
+        document.title = "Reset Password";
+    }
+});
+Router.route("/forgotPassword", {
+    layoutTemplate: "no-layout",
+    onAfterAction: function () {
+        document.title = "Forgot Password";
+    }
 });
 
 Router.route("/search", {
-  action: function() {
-    Router.go("/");
-  }
+    action: function () {
+        Router.go("/");
+    }
 });
 
 var animateContentOut = function () {
         setTimeout(function () {
             $('.landing-form').removeClass("animated fadeIn");
-        },10);
+        }, 10);
         this.next()
     },
     fadeContentIn = function () {
         setTimeout(function () {
             $('.landing-form').addClass("animated fadeIn");
-        },10);
+        }, 10);
     }
 // define this as a global onBeforeAction so it happens all the time
 Router.onBeforeAction(animateContentOut)
--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 Meteor.startup(function () {
+    Accounts.urls.resetPassword = function (token) {
+        return Meteor.absoluteUrl('resetPassword/' + token);
+    };
+    var smtp = {
+        username: 'teamalphameteor@gmail.com',   // eg: server@gentlenode.com
+        password: 'JaJuJuRy',   // eg: 3eeP1gtizk5eziohfervU
+        server: 'smtp.gmail.com',  // eg: mail.gandi.net
+        port: 25
+    }
+
+    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
 
 });
 Meteor.methods({
