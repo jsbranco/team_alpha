db394c91fd2eabe82565011851cebfa34fea5cf9
juniobranco <Br@anco04>
2015-10-11 12:17:57 +0800

    Adding new Meteor.call for adding friends

--- server/friends.js
+++ server/friends.js
@@ -5,11 +5,11 @@ Meteor.methods({
             throw new Meteor.Error("user-id-missing", "User ID is required.");
 
         }
-        if (Meteor.user().following.indexOf(userId) >= 0) {
+        if (Meteor.user().profile.following.indexOf(userId) >= 0) {
             throw new Meteor.Error("already-following", "Already following this user");
         }
 
-        Meteor.users.update({_id: Meteor.user()._id}, {$push: {"following": userId}}, function (err, doc) {
+        Meteor.users.update({_id: Meteor.user()._id}, {$push: {"profile.following": userId}}, function (err, doc) {
             if (err)
                 throw new Meteor.Error("error-updating", err.message);
         })
@@ -19,11 +19,11 @@ Meteor.methods({
         if (userId == undefined) {
             throw new Meteor.Error("user-id-missing", "User ID is required.");
         }
-        if (Meteor.user().following.indexOf(userId) >= 0) {
+        if (Meteor.user().profile.following.indexOf(userId) >= 0) {
             throw new Meteor.Error("not-following-user", "User not in the 'following' list");
         }
 
-        Meteor.users.update({_id: Meteor.user()._id}, {$pull: {"following": userId}}, function (err, doc) {
+        Meteor.users.update({_id: Meteor.user()._id}, {$pull: {"profile.following": userId}}, function (err, doc) {
             if (err)
                 throw new Meteor.Error("error-updating", err.message);
 
