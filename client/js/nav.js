fe9b113c6c8aaf63d9b6aa3f5fc056074796fe05
Jason Wun <jwun@Jasons-MacBook-Pro.local>
2015-10-11 12:25:53 +0800

    Drawer done

---
+++ client/js/nav.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	console.log($(".drawer"));
+  $(".drawer").drawer();
+});
+
+Template.nav.events({
+	"click .drawer-hamburger": function (){
+	console.log("buttonclicked");	
+	  $(".drawer").drawer("toggle");
+	  $(".drawer-hamburger").hide();
+	},
+		"click .closeButton": function (){
+	console.log("closeclicked");	
+	  $(".drawer").drawer("toggle");
+	  	  $(".drawer-hamburger").show();
+	},
+		"drawer.closed .drawer": function (){
+	console.log("closed")
+	},
+	"drawer.opened .drawer": function (){
+	console.log("opened")
+		}
+});
