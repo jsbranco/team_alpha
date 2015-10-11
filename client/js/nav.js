873a8572a96deb31cc1eb78adfbe8c086050e020
juniobranco <Br@anco04>
2015-10-11 23:44:06 +0800

    Adding new Meteor.call for adding friends

--- client/js/nav.js
+++ client/js/nav.js
@@ -1,23 +1,28 @@
-$(document).ready(function() {
-	console.log($(".drawer"));
-  $(".drawer").drawer();
+$(document).ready(function () {
+    console.log($(".drawer"));
+    $(".drawer").drawer();
 });
 
 Template.nav.events({
-	"click .drawer-hamburger": function (){
-	console.log("buttonclicked");	
-	  $(".drawer").drawer("toggle");
-	  $(".drawer-hamburger").hide();
-	},
-		"click .closeButton": function (){
-	console.log("closeclicked");	
-	  $(".drawer").drawer("toggle");
-	  	  $(".drawer-hamburger").show();
-	},
-		"drawer.closed .drawer": function (){
-	console.log("closed")
-	},
-	"drawer.opened .drawer": function (){
-	console.log("opened")
-		}
+    "click .drawer-hamburger": function () {
+        console.log("buttonclicked");
+        $(".drawer").drawer("toggle");
+        $(".drawer-hamburger").hide();
+    },
+    "click .closeButton": function () {
+        console.log("closeclicked");
+        $(".drawer").drawer("toggle");
+        $(".drawer-hamburger").show();
+    },
+    "click .drawer-menu  li a": function () {
+        $(".drawer").drawer("toggle");
+        $(".drawer-hamburger").show();
+
+    },
+    "drawer.closed .drawer": function () {
+        console.log("closed")
+    },
+    "drawer.opened .drawer": function () {
+        console.log("opened")
+    }
 });
